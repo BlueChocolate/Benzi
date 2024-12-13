25291682e2fb3674a0b6314d825429a0f895c79f
RadioNoise <ktche22y@outlook.com>
2024-12-13 22:25:15 +0800

    更换为 MudBlazor 项目模板

--- Benzi/Benzi.Client/Program.cs
+++ Benzi/Benzi.Client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace Benzi.Client
 {
@@ -7,6 +8,10 @@ namespace Benzi.Client
         static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
+
 
             await builder.Build().RunAsync();
         }
--- Benzi/Benzi/Program.cs
+++ Benzi/Benzi/Program.cs
@@ -1,5 +1,6 @@
 using Benzi.Client.Pages;
 using Benzi.Components;
+using MudBlazor.Services;
 
 namespace Benzi
 {
@@ -8,6 +9,9 @@ namespace Benzi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add MudBlazor services
+            builder.Services.AddMudServices();
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
